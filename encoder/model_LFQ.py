5a66c9e45e47683ad5eaf8e45a72bc338ff70247
Shujie HU (胡书杰) <33870431+XiaoshanHsj@users.noreply.github.com>
2024-11-26 20:49:36 +0800

    Update model_LFQ.py

--- encoder/model_LFQ.py
+++ encoder/model_LFQ.py
@@ -179,8 +179,8 @@ class EncodecModel_LFQ(nn.Module):
 
     def _decode_frame(self, encoded_frame: EncodedFrame) -> torch.Tensor:
         codes, scale = encoded_frame
-        # codes = codes.transpose(0, 1)
         emb = self.quantizer.decode(codes)
+        # import pdb; pdb.set_trace()
         emb = rearrange(emb, '(b n) d -> b d n', n = emb.shape[0])
         # import pdb; pdb.set_trace()
         out = self.decoder(emb)
